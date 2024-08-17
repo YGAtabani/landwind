20cebb3d8eeb68131dca94e37d9820916110185e
Your Name <yousfiatabanii@gmail.com>
2024-08-17 05:54:51 +0100

    added routes

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
